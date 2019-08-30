f58f2853b87f8e019870a6586db887e0518e8f42
harimasora <zidalo@gmail.com>
2019-08-30 16:17:06 -0300

    feat: Merge code from presentation mockup

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,245 +2,83 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
 	{
-		title: 'E-commerce',
-		icon: 'shopping-cart-outline',
-		link: '/pages/dashboard',
-		home: true,
-	},
-	{
-		title: 'IoT Dashboard',
-		icon: 'home-outline',
-		link: '/pages/iot-dashboard',
-	},
-	{
-		title: 'FEATURES',
+		title: 'GERAL',
 		group: true,
 	},
 	{
-		title: 'Layout',
-		icon: 'layout-outline',
+		title: 'Dashboard',
+		icon: 'nb-home',
+		link: '/pages/dashboard',
+	},
+	{
+		title: 'TESOURARIA',
+		group: true,
+	},
+	{
+		title: 'Fluxo de caixa',
+		icon: 'nb-bar-chart',
+	},
+	{
+		title: 'Controle de inventário',
+		icon: 'nb-checkmark',
+	},
+	{
+		title: 'ADMINISTRATIVO',
+		group: true,
+	},
+	{
+		title: 'Controle de ATAs',
+		icon: 'nb-compose',
+	},
+	{
+		title: 'Controle de acessos',
+		icon: 'nb-locked',
+	},
+	{
+		title: 'Controle de pessoas',
+		icon: 'nb-person',
+	},
+	{
+		title: 'JURÍDICO',
+		group: true,
+	},
+	{
+		title: 'Processos',
+		icon: 'nb-list',
+	},
+	{
+		title: 'SISTEMA',
+		group: true,
+	},
+	{
+		title: 'Autenticação',
+		icon: 'nb-locked',
 		children: [
 			{
-				title: 'Stepper',
-				link: '/pages/layout/stepper',
+				title: 'Entrar',
+				link: '/auth/login',
 			},
 			{
-				title: 'List',
-				link: '/pages/layout/list',
+				title: 'Registrar',
+				link: '/auth/register',
 			},
 			{
-				title: 'Infinite List',
-				link: '/pages/layout/infinite-list',
+				title: 'Esqueci minha senha',
+				link: '/auth/request-password',
 			},
 			{
-				title: 'Accordion',
-				link: '/pages/layout/accordion',
-			},
-			{
-				title: 'Tabs',
-				pathMatch: 'prefix',
-				link: '/pages/layout/tabs',
+				title: 'Nova Senha',
+				link: '/auth/reset-password',
 			},
 		],
 	},
 	{
-		title: 'Forms',
-		icon: 'edit-2-outline',
-		children: [
-			{
-				title: 'Form Inputs',
-				link: '/pages/forms/inputs',
-			},
-			{
-				title: 'Form Layouts',
-				link: '/pages/forms/layouts',
-			},
-			{
-				title: 'Buttons',
-				link: '/pages/forms/buttons',
-			},
-			{
-				title: 'Datepicker',
-				link: '/pages/forms/datepicker',
-			},
-		],
-	},
-	{
-		title: 'UI Features',
-		icon: 'keypad-outline',
-		link: '/pages/ui-features',
-		children: [
-			{
-				title: 'Grid',
-				link: '/pages/ui-features/grid',
-			},
-			{
-				title: 'Icons',
-				link: '/pages/ui-features/icons',
-			},
-			{
-				title: 'Typography',
-				link: '/pages/ui-features/typography',
-			},
-			{
-				title: 'Animated Searches',
-				link: '/pages/ui-features/search-fields',
-			},
-		],
-	},
-	{
-		title: 'Modal & Overlays',
-		icon: 'browser-outline',
-		children: [
-			{
-				title: 'Dialog',
-				link: '/pages/modal-overlays/dialog',
-			},
-			{
-				title: 'Window',
-				link: '/pages/modal-overlays/window',
-			},
-			{
-				title: 'Popover',
-				link: '/pages/modal-overlays/popover',
-			},
-			{
-				title: 'Toastr',
-				link: '/pages/modal-overlays/toastr',
-			},
-			{
-				title: 'Tooltip',
-				link: '/pages/modal-overlays/tooltip',
-			},
-		],
-	},
-	{
-		title: 'Extra Components',
-		icon: 'message-circle-outline',
-		children: [
-			{
-				title: 'Calendar',
-				link: '/pages/extra-components/calendar',
-			},
-			{
-				title: 'Progress Bar',
-				link: '/pages/extra-components/progress-bar',
-			},
-			{
-				title: 'Spinner',
-				link: '/pages/extra-components/spinner',
-			},
-			{
-				title: 'Alert',
-				link: '/pages/extra-components/alert',
-			},
-			{
-				title: 'Calendar Kit',
-				link: '/pages/extra-components/calendar-kit',
-			},
-			{
-				title: 'Chat',
-				link: '/pages/extra-components/chat',
-			},
-		],
-	},
-	{
-		title: 'Maps',
-		icon: 'map-outline',
-		children: [
-			{
-				title: 'Google Maps',
-				link: '/pages/maps/gmaps',
-			},
-			{
-				title: 'Leaflet Maps',
-				link: '/pages/maps/leaflet',
-			},
-			{
-				title: 'Bubble Maps',
-				link: '/pages/maps/bubble',
-			},
-			{
-				title: 'Search Maps',
-				link: '/pages/maps/searchmap',
-			},
-		],
-	},
-	{
-		title: 'Charts',
-		icon: 'pie-chart-outline',
-		children: [
-			{
-				title: 'Echarts',
-				link: '/pages/charts/echarts',
-			},
-			{
-				title: 'Charts.js',
-				link: '/pages/charts/chartjs',
-			},
-			{
-				title: 'D3',
-				link: '/pages/charts/d3',
-			},
-		],
-	},
-	{
-		title: 'Editors',
-		icon: 'text-outline',
-		children: [
-			{
-				title: 'TinyMCE',
-				link: '/pages/editors/tinymce',
-			},
-			{
-				title: 'CKEditor',
-				link: '/pages/editors/ckeditor',
-			},
-		],
-	},
-	{
-		title: 'Tables & Data',
-		icon: 'grid-outline',
-		children: [
-			{
-				title: 'Smart Table',
-				link: '/pages/tables/smart-table',
-			},
-			{
-				title: 'Tree Grid',
-				link: '/pages/tables/tree-grid',
-			},
-		],
-	},
-	{
-		title: 'Miscellaneous',
-		icon: 'shuffle-2-outline',
+		title: 'Variados',
+		icon: 'nb-shuffle',
 		children: [
 			{
 				title: '404',
 				link: '/pages/miscellaneous/404',
-			},
-		],
-	},
-	{
-		title: 'Auth',
-		icon: 'lock-outline',
-		children: [
-			{
-				title: 'Login',
-				link: '/auth/login',
-			},
-			{
-				title: 'Register',
-				link: '/auth/register',
-			},
-			{
-				title: 'Request Password',
-				link: '/auth/request-password',
-			},
-			{
-				title: 'Reset Password',
-				link: '/auth/reset-password',
 			},
 		],
 	},
