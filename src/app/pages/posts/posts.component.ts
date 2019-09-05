ff20c2de23a48d2805ef8ab645093071b0de8f74
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2019-09-05 17:16:02 -0300

    feat(posts): Implement service with CRUD operations for post

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { Post, PostsService } from './../../services/posts.service';
 
 @Component({
 	selector: 'ngx-posts',
@@ -8,10 +8,55 @@ import { Observable } from 'rxjs';
 	styleUrls: [ './posts.component.scss' ],
 })
 export class PostsComponent implements OnInit {
-	items: Observable<any[]>;
-	constructor(db: AngularFirestore) {
-		this.items = db.collection('items').valueChanges();
-	}
+  posts: any[];
+  searchedPosts: any[];
+  prevKeys: number[] = [];
+  nextKey: number;
+  titleQuery$: Subject<string>;
+  textField: string;
 
-	ngOnInit() {}
+  constructor(public postsService: PostsService) {
+    // subscribe to changes - input dynamic search
+    this.titleQuery$ = new Subject<string>();
+    const queryObservable = this.postsService.dynamicSearch(this.titleQuery$, 'title', 5);
+    queryObservable.subscribe(queriedItems => {
+      this.searchedPosts = queriedItems;
+    });
+  }
+
+  ngOnInit() {
+    this.postsService.getFirstPost().then( (firstPost) => {
+      this.getPosts(firstPost[0].createdAt);
+    });
+  }
+
+  public async createPost(title: string) {
+    const post: Post = {
+      'title': title,
+      createdAt: 0,
+    };
+    const res = await this.postsService.createPost(post);
+  }
+
+  previousPage() {
+    this.getPosts(this.prevKeys.pop());
+  }
+
+  nextPage() {
+    this.prevKeys.push(this.posts[0].createdAt);
+    this.getPosts(this.nextKey);
+  }
+
+  getPosts(startAt) {
+    this.postsService.listPosts(6, startAt).then(
+      (list) => {
+        if (list.length === 6) {
+          this.nextKey = list.pop().createdAt;
+        } else {
+          this.nextKey = null;
+        }
+        this.posts = list;
+      },
+    );
+  }
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,100 @@
+import { take, switchMap } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import { Subject, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+
+export class PostsService {
+
+  private collection: AngularFirestoreCollection<Post>;
+
+  constructor( private afs: AngularFirestore) {
+    this.collection = this.afs.collection('posts');
+  }
+
+  /**
+   * @param post The post object complying to Post interface to be inserted in the Firestore
+   */
+  public createPost(post: Post) {
+    post.createdAt = Date.now();
+    return  this.collection.add(post);
+  }
+
+  public updatePost(id: string, post: Post) {
+    return this.collection.doc<Post>(`${id}`).update(post);
+  }
+
+  public deletePost(id: string) {
+    return this.collection.doc(`${id}`).delete();
+  }
+
+  public getPost(id: string) {
+    return this.collection.doc(`${id}`).valueChanges().pipe(take(1)).toPromise();
+  }
+
+  public listPosts(offset: number, startAt?: number) {
+     return this.afs.collection<Post>('posts', ref => ref.orderBy('createdAt', 'desc').startAt(startAt).limit(offset))
+     .valueChanges({'idField': 'id'})
+     .pipe(take(1)) // this is used to make the observer emmit and END signal, so we can convert to promise
+     .toPromise();
+  }
+
+  public search(propertyToCompare: string, queryString: string, orderedby: string = propertyToCompare) {
+    return this.afs.collection<Post>('posts', ref => ref.where(`${propertyToCompare}`, '==', queryString)
+    .orderBy(orderedby))
+    .valueChanges()
+    .pipe(take(1))
+    .toPromise();
+  }
+
+  // Helper methods
+  public getFirstPost(fieldToOrder: string = 'createdAt', directionStr: firebase.firestore.OrderByDirection = 'desc') {
+    return this.afs.collection<Post>('posts', ref => ref.orderBy(fieldToOrder, directionStr).limit(1))
+    .valueChanges({'idField': 'id'})
+    .pipe(take(1))
+    .toPromise();
+  }
+
+  dynamicSearch(input$: Subject<string>, propertyToCompare: string, offset: number = 15) {
+    const queryObservable = input$.pipe(
+      switchMap(value => {
+          if (!value) {
+            return of([]);
+          } else {
+            return this.afs.collection('posts', ref => {
+              return ref.orderBy(propertyToCompare).startAt(value).endAt(value + '\uf8ff').limit(offset);
+            }).valueChanges();
+          }
+        },
+      ),
+    );
+    return queryObservable;
+  }
+
+}
+
+export interface Post {
+  // id: string;
+  title: string;
+  createdAt: number;
+  // resumo: string;
+  // fonte: string;
+  // txPublicacao: string;
+  // imgPrincipal?: string;
+  // slides?: [string];
+  // arquivos?: [string];
+  // dhCad: string;
+  // userCad: string;
+  // dhUpd: string;
+  // userUpd: string;
+  // dhPublicacao: string;
+  // dhVigência?: string;
+  // areaSite: string;
+  // destaque: boolean;
+  // tags: string;
+  // estado: string;//cadastrado, cancelado, publicado, suspenso, não vigente
+}
