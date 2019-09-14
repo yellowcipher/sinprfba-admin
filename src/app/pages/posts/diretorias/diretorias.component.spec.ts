c445dc3266885d0b6639b8cceb94d30517ef84a0
harimasora <zidalo@gmail.com>
2019-09-13 22:04:48 -0300

    style: Fix trailing commas on diretorias

--- src/app/pages/posts/diretorias/diretorias.component.spec.ts
+++ src/app/pages/posts/diretorias/diretorias.component.spec.ts
@@ -3,23 +3,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DiretoriasComponent } from './diretorias.component';
 
 describe('DiretoriasComponent', () => {
-  let component: DiretoriasComponent;
-  let fixture: ComponentFixture<DiretoriasComponent>;
+	let component: DiretoriasComponent;
+	let fixture: ComponentFixture<DiretoriasComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DiretoriasComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				declarations: [ DiretoriasComponent ],
+			}).compileComponents();
+		}),
+	);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DiretoriasComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(DiretoriasComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/pages/posts/diretorias/diretorias.component.ts
+++ src/app/pages/posts/diretorias/diretorias.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ngx-diretorias',
-  templateUrl: './diretorias.component.html',
-  styleUrls: ['./diretorias.component.scss']
+	selector: 'ngx-diretorias',
+	templateUrl: './diretorias.component.html',
+	styleUrls: [ './diretorias.component.scss' ],
 })
 export class DiretoriasComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
