9c094b45b5278aff60b9b933fa16ba157e7a8ea9
harimasora <zidalo@gmail.com>
2019-11-05 15:07:30 -0300

    feat(posts): Edit image on post

--- src/app/pages/posts/news/edit-news/edit-news.component.ts
+++ src/app/pages/posts/news/edit-news/edit-news.component.ts
@@ -1,6 +1,6 @@
 import { Post } from './../../../../models/post';
 import { PostsService } from './../../../../services/posts.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
@@ -12,8 +12,18 @@ import { map, filter } from 'rxjs/operators';
 })
 export class EditNewsComponent implements OnInit {
 	post$: Observable<Post>;
+	isEditingImage: boolean = false;
+	fileToUpload: File;
 
-	constructor(private postsService: PostsService, private router: Router, private route: ActivatedRoute) {}
+	@ViewChild('imageInput', { static: false })
+	imageInput: ElementRef;
+
+	constructor(
+		private postsService: PostsService,
+		private router: Router,
+		private route: ActivatedRoute,
+		private changeDetectorRef: ChangeDetectorRef,
+	) {}
 
 	ngOnInit() {
 		const id = this.router.url.split('/').pop();
@@ -23,7 +33,24 @@ export class EditNewsComponent implements OnInit {
 		);
 	}
 
+	startEditing() {
+		this.isEditingImage = true;
+		this.changeDetectorRef.detectChanges();
+	}
+
+	cancelEditing() {
+		this.isEditingImage = false;
+		this.fileToUpload = null;
+		this.changeDetectorRef.detectChanges();
+	}
+
+	onFileChange(files: FileList) {
+		this.imageInput.nativeElement.innerText = Array.from(files).map((f) => f.name).join(', ');
+		this.fileToUpload = files.item(0);
+	}
+
 	async update(post: Post) {
+		post.mainImage = this.fileToUpload;
 		await this.postsService.update(post);
 		this.router.navigate([ '../../' ], { relativeTo: this.route });
 	}
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -33,40 +33,45 @@ export class PostsService {
 			createdAt,
 		};
 
-		// Upload and remove image files
-		if (record.mainImage != null) {
-			recordToSend.mainImageUrl = await this.uploadService.upload(record.mainImage, {
-				folder: uid,
-				filename: this.MAIN_IMAGE_NAME,
-			});
-		}
-		delete recordToSend.mainImage;
+		await this.uploadFiles(recordToSend);
 
-		if (record.slides != null) {
-			recordToSend.slidesUrls = [];
-			for (let i: number = 0; i < record.slides.length; i++) {
-				recordToSend.slidesUrls[i] = await this.uploadService.upload(record.slides[i], {
-					folder: uid,
-					filename: this.buildSlideName(i),
-				});
-			}
-		}
-		delete recordToSend.slides;
 		return this.collection.doc(uid).set(recordToSend, { merge: true });
 	}
 
-	update(record: Post) {
+	async update(record: Post) {
 		const updatedAt = firebase.firestore.FieldValue.serverTimestamp();
 		const recordToUpdate: Post = {
 			...record,
 			updatedAt,
 		};
+		await this.uploadFiles(recordToUpdate);
 		return this.collection.doc(recordToUpdate.uid).set(recordToUpdate, { merge: true });
 	}
 
 	async delete(record: Post): Promise<void> {
 		await this.deleteStorageFiles(record);
 		return this.collection.doc(record.uid).delete();
+	}
+
+	private async uploadFiles(record: Post) {
+		if (record.mainImage != null) {
+			record.mainImageUrl = await this.uploadService.upload(record.mainImage, {
+				folder: record.uid,
+				filename: this.MAIN_IMAGE_NAME,
+			});
+		}
+		delete record.mainImage;
+
+		if (record.slides != null) {
+			record.slidesUrls = [];
+			for (let i: number = 0; i < record.slides.length; i++) {
+				record.slidesUrls[i] = await this.uploadService.upload(record.slides[i], {
+					folder: record.uid,
+					filename: this.buildSlideName(i),
+				});
+			}
+		}
+		delete record.slides;
 	}
 
 	private deleteStorageFiles(record: Post): Promise<any[]> {
