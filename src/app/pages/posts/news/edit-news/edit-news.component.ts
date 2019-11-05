6f217f152acdd71bf956b635560dda388fe65b0b
harimasora <zidalo@gmail.com>
2019-11-05 15:53:10 -0300

    feat(posts): Edit start and end dates

--- src/app/pages/posts/news/edit-news/edit-news.component.ts
+++ src/app/pages/posts/news/edit-news/edit-news.component.ts
@@ -3,7 +3,8 @@ import { PostsService } from './../../../../services/posts.service';
 import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'ngx-edit-news',
@@ -14,6 +15,8 @@ export class EditNewsComponent implements OnInit {
 	post$: Observable<Post>;
 	isEditingImage: boolean = false;
 	fileToUpload: File;
+	startDate: FormControl;
+	endDate: FormControl;
 
 	@ViewChild('imageInput', { static: false })
 	imageInput: ElementRef;
@@ -30,6 +33,23 @@ export class EditNewsComponent implements OnInit {
 		this.post$ = this.postsService.records.pipe(
 			filter((records) => records.length > 0),
 			map((records) => records.find((record) => record.uid === id)),
+			tap((post) => {
+				if (post.startDate != null) {
+					this.startDate = new FormControl(
+						post.startDate instanceof Date ? post.startDate : (post.startDate as any).toDate(),
+					);
+				} else {
+					this.startDate = new FormControl();
+				}
+
+				if (post.endDate != null) {
+					this.endDate = new FormControl(
+						post.endDate instanceof Date ? post.endDate : (post.endDate as any).toDate(),
+					);
+				} else {
+					this.endDate = new FormControl();
+				}
+			}),
 		);
 	}
 
@@ -51,6 +71,8 @@ export class EditNewsComponent implements OnInit {
 
 	async update(post: Post) {
 		post.mainImage = this.fileToUpload;
+		post.startDate = this.startDate.value;
+		post.endDate = this.endDate.value || null;
 		await this.postsService.update(post);
 		this.router.navigate([ '../../' ], { relativeTo: this.route });
 	}
